99cf9677c87225c6fead51229dfcfda7168f9e87
yubo <yubo@133.cn>
2018-08-01 16:01:34 +0800

    update

---
+++ lib/api/Api.dart
@@ -0,0 +1,5 @@
+
+class Api {
+  // 资讯列表
+  static final String NEWS_LIST = "http://osc.yubo725.top/news/list";
+}
--- lib/pages/NewsListPage.dart
+++ lib/pages/NewsListPage.dart
@@ -1,53 +1,150 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_osc/widgets/SlideView.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter_osc/api/Api.dart';
+import 'package:flutter_osc/util/NetUtils.dart';
+import 'package:flutter_osc/constants/Constants.dart';
 
 // 资讯列表页面
-class NewsListPage extends StatelessWidget {
+class NewsListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => NewsListPageState();
+}
+
+class NewsListPageState extends State<NewsListPage> {
 
   // 轮播图的数据
-  var slideData = [];
+  var slideData = null;
   // 列表的数据（轮播图数据和列表数据分开，但是实际上轮播图和列表中的item同属于ListView的item）
-  var listData = [];
+  var listData = null;
   // 列表中资讯标题的样式
   TextStyle titleTextStyle = new TextStyle(fontSize: 15.0);
   // 时间文本的样式
   TextStyle subtitleStyle = new TextStyle(color: const Color(0xFFB5BDC0), fontSize: 12.0);
+  // 当前数据页索引值
+  var curPage = 1;
+  // 数据总条数
+  var listTotalSize = 0;
+  // ListView控制器
+  ScrollController _controller = new ScrollController();
 
-  NewsListPage() {
-    // 这里做数据初始化，加入一些测试数据
-    for (int i = 0; i < 3; i++) {
-      Map map = new Map();
-      // 轮播图的资讯标题
-      map['title'] = 'Python 之父透露退位隐情，与核心开发团队产生隔阂';
-      // 轮播图的详情URL
-      map['detailUrl'] = 'https://www.oschina.net/news/98455/guido-van-rossum-resigns';
-      // 轮播图的图片URL
-      map['imgUrl'] = 'https://static.oschina.net/uploads/img/201807/30113144_1SRR.png';
-      slideData.add(map);
-    }
-    for (int i = 0; i < 30; i++) {
-      Map map = new Map();
-      // 列表item的标题
-      map['title'] = 'J2Cache 2.3.23 发布，支持 memcached 二级缓存';
-      // 列表item的作者头像URL
-      map['authorImg'] = 'https://static.oschina.net/uploads/user/0/12_50.jpg?t=1421200584000';
-      // 列表item的时间文本
-      map['timeStr'] = '2018/7/30';
-      // 列表item的资讯图片
-      map['thumb'] = 'https://static.oschina.net/uploads/logo/j2cache_N3NcX.png';
-      // 列表item的评论数
-      map['commCount'] = 5;
-      listData.add(map);
-    }
+  NewsListPageState() {
+//    // 这里做数据初始化，加入一些测试数据
+//    for (int i = 0; i < 3; i++) {
+//      Map map = new Map();
+//      // 轮播图的资讯标题
+//      map['title'] = 'Python 之父透露退位隐情，与核心开发团队产生隔阂';
+//      // 轮播图的详情URL
+//      map['detailUrl'] = 'https://www.oschina.net/news/98455/guido-van-rossum-resigns';
+//      // 轮播图的图片URL
+//      map['imgUrl'] = 'https://static.oschina.net/uploads/img/201807/30113144_1SRR.png';
+//      slideData.add(map);
+//    }
+//    for (int i = 0; i < 30; i++) {
+//      Map map = new Map();
+//      // 列表item的标题
+//      map['title'] = 'J2Cache 2.3.23 发布，支持 memcached 二级缓存';
+//      // 列表item的作者头像URL
+//      map['authorImg'] = 'https://static.oschina.net/uploads/user/0/12_50.jpg?t=1421200584000';
+//      // 列表item的时间文本
+//      map['timeStr'] = '2018/7/30';
+//      // 列表item的资讯图片
+//      map['thumb'] = 'https://static.oschina.net/uploads/logo/j2cache_N3NcX.png';
+//      // 列表item的评论数
+//      map['commCount'] = 5;
+//      listData.add(map);
+//    }
+    _controller.addListener(() {
+      var maxScroll = _controller.position.maxScrollExtent;
+      var pixels = _controller.position.pixels;
+      if (maxScroll == pixels && listData.length < listTotalSize) {
+        // scroll to bottom, get next page data
+        print("load more ... ");
+        curPage++;
+        getNewsList(true);
+      }
+    });
+  }
+
+  Future<Null> _pullToRefresh() async {
+    curPage = 1;
+    getNewsList(false);
+    return null;
+  }
+
+  // 从网络获取数据，isLoadMore表示是否是加载更多数据
+  getNewsList(bool isLoadMore) {
+    String url = Api.NEWS_LIST;
+    url += "?pageIndex=$curPage&pageSize=10";
+    NetUtils.get(url).then((data) {
+      if (data != null) {
+        // 将接口返回的json字符串解析为map类型
+        Map<String, dynamic> map = json.decode(data);
+        if (map['code'] == 0) {
+          // code=0表示请求成功
+          var msg = map['msg'];
+          // total表示资讯总条数
+          listTotalSize = msg['news']['total'];
+          // data为数据内容，其中包含slide和news两部分，分别表示头部轮播图数据，和下面的列表数据
+          var _listData = msg['news']['data'];
+          var _slideData = msg['slide'];
+          setState(() {
+            if (!isLoadMore) {
+              // 不是加载更多，则直接为变量赋值
+              listData = _listData;
+              slideData = _slideData;
+            } else {
+              // 是加载更多，则需要将取到的news数据追加到原来的数据后面
+              List list1 = new List();
+              // 添加原来的数据
+              list1.addAll(listData);
+              // 添加新取到的数据
+              list1.addAll(_listData);
+              // 判断是否获取了所有的数据，如果是，则需要显示底部的"我也是有底线的"布局
+              if (list1.length >= listTotalSize) {
+                list1.add(Constants.END_LINE_TAG);
+              }
+              // 给列表数据赋值
+              listData = list1;
+              // 轮播图数据
+              slideData = _slideData;
+            }
+          });
+        }
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getNewsList(false);
   }
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      // 这里itemCount是将轮播图组件、分割线和列表items都作为ListView的item算了
-      itemCount: listData.length * 2 + 1,
-      itemBuilder: (context, i) => renderRow(i)
-    );
+    // 无数据时，显示Loading
+    if (listData == null) {
+      print("show loading...");
+      return new Center(
+        // CircularProgressIndicator是一个圆形的Loading进度条
+        child: new CircularProgressIndicator(),
+      );
+    } else {
+      // 有数据，显示ListView
+      Widget listView = new ListView.builder(
+        itemCount: listData.length * 2,
+        itemBuilder: (context, i) => renderRow(i),
+        controller: _controller,
+      );
+      return new RefreshIndicator(child: listView, onRefresh: _pullToRefresh);
+    }
+//    return new ListView.builder(
+//      // 这里itemCount是将轮播图组件、分割线和列表items都作为ListView的item算了
+//      itemCount: listData.length * 2 + 1,
+//      itemBuilder: (context, i) => renderRow(i)
+//    );
   }
 
   // 渲染列表item
